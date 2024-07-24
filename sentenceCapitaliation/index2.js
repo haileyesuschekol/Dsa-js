46ff5b89ea49937ec82719a87fbb49c01bcf3f85
Haileyesus Chekol <teamdevs7@gmail.com>
2024-07-24 18:42:06 +0100

    solution 2 for capitalization

---
+++ sentenceCapitaliation/index2.js
@@ -0,0 +1,10 @@
+const capital = (str) => {
+  const words = []
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return words.join(" ")
+}
+
+console.log(capital("hello"))
