ce3a98ddccb3581b3336e9a2f77ffa9cdaaa4c30
Haileyesus Chekol <teamdevs7@gmail.com>
2024-07-26 21:00:32 +0100

    rename file name

--- sortObject-in-array/index.js
+++ sortObject-in-array/index.js
@@ -11,9 +11,8 @@ const sortAlgo = (data) => {
 data.sort((a, b) => {
   const val1 = sortAlgo(a)
   const val2 = sortAlgo(b)
-  console.log(val1, val2)
   if (typeof val1 === "string") {
-    return val1.localeCompare(val2)
+    return val1.localeCompare(val2) * order
   } else {
     return val1 - val2
   }
